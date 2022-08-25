6172de634ad8a2035e212a4040f18db57b976c9e
jeong <roy1109@naver.com>
2022-08-26 03:37:05 +0900

    Feat : Main

---
+++ app/src/main/java/com/example/applepiexhibitionchattingapplication/AddChatRoomActivity.kt
@@ -0,0 +1,17 @@
+package com.example.applepiexhibitionchattingapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class AddChatRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_chat_room)
+        val backBtn = findViewById<ImageButton>(R.id.addBackImageButton)
+        backBtn.setOnClickListener {
+            startActivity(Intent(this@AddChatRoomActivity,MainActivity::class.java))
+        }
+    }
+}

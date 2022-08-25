04551c6fe9b60f9c7f03fbe7ec6ff03a648d650d
sanghyeonjung <roy1109@naver.com>
2022-08-25 17:20:16 +0900

    Feat : Option

--- app/src/main/java/com/example/applepiexhibitionchattingapplication/OptionActivity.kt
+++ app/src/main/java/com/example/applepiexhibitionchattingapplication/OptionActivity.kt
@@ -1,11 +1,35 @@
 package com.example.applepiexhibitionchattingapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class OptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_option)
+        val optionBackBtn = findViewById<ImageButton>(R.id.optionBackImageButton)
+        val myNameEditText = findViewById<EditText>(R.id.optionMyNameSetEdittext)
+        val db = Firebase.firestore
+        db.collection("user")
+            .get()
+            .addOnCompleteListener{ result ->
+                for(document in result){
+                    myNameEditText.setText(document["id"] as String)
+                }
+            }
+            .addOnFailureListener{ exception ->
+                Log.e("OptionActivity","Error: $exception")
+
+            }
+        optionBackBtn.setOnClickListener {
+            startActivity(Intent(this@OptionActivity,MainActivity::class.java))
+        }
     }
 }
